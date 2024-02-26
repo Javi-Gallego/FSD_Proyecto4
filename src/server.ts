13dbc5902db244a15589d1ea4b43924d3ad0efa2
Javi-Gallego <galgar@gmail.com>
2024-02-26 21:24:18 +0100

    feat: reach server through thunder client at endpoint healthy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 
 import 'dotenv/config'
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 
 
 const PORT = process.env.PORT || 4001
@@ -9,6 +9,13 @@ console.log(PORT)
 const app:Application = express()
 
 app.use(express.json());
+
+app.get("/healthy", (req: Request, res: Response) => {
+    res.status(200).json({
+        success: true,
+        message: "Server is healthy",
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
