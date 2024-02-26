4ec6d98d521469feb922e912c4058a48b20575ec
Javi-Gallego <galgar@gmail.com>
2024-02-26 21:30:35 +0100

    feat: divide server and app

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,9 @@
 
 import 'dotenv/config'
-import express, { Application, Request, Response } from 'express'
+import { app } from './app'
 
 
 const PORT = process.env.PORT || 4001
-console.log(PORT)
-
-const app:Application = express()
-
-app.use(express.json());
-
-app.get("/healthy", (req: Request, res: Response) => {
-    res.status(200).json({
-        success: true,
-        message: "Server is healthy",
-    })
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
